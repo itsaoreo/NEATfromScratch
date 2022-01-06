d7f6b91d96674fe1cdb27e8fe8ca9989bd6aa02e
David Zhi LuoZhang <51495738+davidluozhang@users.noreply.github.com>
2022-01-05 23:34:48 -0800

    Refactored innov_num -> innovation_num in ConnectionHistory for consistency between Connection and ConnectionHistory

--- ConnectionHistory.py
+++ ConnectionHistory.py
@@ -7,10 +7,10 @@ class ConnectionHistory:
         self.innovation_num_list = innov_num_list.copy()
 
     def equals(self, genome, from_node, to_node):
-        if len(genome.genes) == len(self.innov_num_list):
+        if len(genome.genes) == len(self.innovation_num_list):
             if from_node.id == self.from_node_id and to_node.id == self.to_node_id:
                 for i in range(len(genome.genes)):
-                    if genome.genes[i].innovation_num in self.innov_num_list:
+                    if genome.genes[i].innovation_num in self.innovation_num_list:
                         return False
                 return True
         return False
