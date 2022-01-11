ad36446cfbc481ea79a9f4ad544d3a78ffd02148
David Zhi LuoZhang <51495738+davidluozhang@users.noreply.github.com>
2022-01-10 18:14:34 -0800

    generate network, delete template, comments

--- EvolutionStep.py
+++ EvolutionStep.py
@@ -6,11 +6,15 @@ class EvolutionStep:
         self.innovation_num = innov_num
         self.innovation_num_list = innov_num_list.copy()
 
+    # check if a step in evolution (ie an evolved structure) has already evolved before
     def equals(self, genome, from_node, to_node):
+        # identical structures must have the same number of innovations ie number of added nodes and connections
         if len(genome.genes) == len(self.innovation_num_list):
+            # todo: do we actually need to check to and from?
             if from_node.id == self.from_node_id and to_node.id == self.to_node_id:
+                # check if innovation numbers are the same
                 for i in range(len(genome.genes)):
-                    if genome.genes[i].innovation_num in self.innovation_num_list:
+                    if genome.genes[i].innovation_num not in self.innovation_num_list:
                         return False
                 return True
         return False
--- Genome.py
+++ Genome.py
@@ -89,7 +89,7 @@ class Genome:
             for i in range(len(self.nodes) - 1):
                 if self.nodes[i].layer >= self.get_node(newNodeNumber):
                     self.nodes[i].layer += 1
-            self.layers += 1
+            self.layers_num += 1
 
         self.connect_nodes()
         return
@@ -118,7 +118,7 @@ class Genome:
             self.genes[i].node_from.output_connections.append(self.genes[i])
 
     # Mutation method #2: Generate a random connection
-    def add_connection(self, innovation_history_list):
+    def add_connection(self, evolution_history):
         # Check if the neural net is full ie no new connections can be added
         if self.is_full():
             print("Network full. Add connection failed.")
@@ -138,7 +138,7 @@ class Genome:
         to_node = self.genes[to_index]
 
         # Get the innovation number for this mutation
-        connection_innov_num = self.get_innov_num(from_node, to_node)
+        connection_innov_num = self.get_innov_num(evolution_history, from_node, to_node)
         # Add this connection with a random weight and the innovation number to the list of connections
         self.genes.append(Connection(from_node, to_node, random.uniform(-1, 1), connection_innov_num))
         # Connect nodes
@@ -206,3 +206,14 @@ class Genome:
             innovation_num_list.append(self.genes[i].innovation_num)
         new_evolution = EvolutionStep(from_node_id, to_node_id, connection_innov_num, innovation_num_list)
         evolution_history.append(new_evolution)
+
+    # represent network as a list of nodes waiting to be activated
+    def generate_network(self):
+        self.connect_nodes()
+        network = []
+
+        # for each layer, go through all nodes and add the nodes that are in that layer
+        for i in range(self.layers_num):
+            for j in range(len(self.nodes)):
+                if self.nodes[i].layer == i:
+                    network.append(self.nodes[i])
